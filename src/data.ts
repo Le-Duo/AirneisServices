cbc5c74ac64c0b1c5be632cb7b3eb4de36bfe681
JenniferToussaint <toussaint.jennifer@gmail.com>
2023-12-27 21:41:14 +0100

    add SLUG in data.ts for tests + adapt category

--- src/data.ts
+++ src/data.ts
@@ -4,74 +4,86 @@
  * Les produits et les utilisateurs sont définis avec leurs attributs respectifs, tels que le nom, l'image, la catégorie, la marque, le prix, le stock pour les produits et le nom, l'email, le mot de passe pour les utilisateurs.
  */
 
-import { Category } from './models/category'
+// ONLY FOR TEST //
+import { DocumentType } from '@typegoose/typegoose'
+import { Category, CategoryModel } from './models/category'
 import { Product } from './models/product'
 import { User } from './models/user'
 import bcrypt from 'bcryptjs'
 
+
+
 export const sampleProducts: Product[] = [
   {
     name: 'MACLEOD',
+    slug: 'macleod',
     URLimage: '../public/images/blue-3-seater-sofa.png',
-    category: new Category("Sofa", "./images/categories/sofa"),
+    category: (CategoryModel.findById("658ad8a0c5f1cecc0b52d46d")) as unknown as DocumentType<Category>, //récupère une categorie dont l'id est 658ad8a0c5f1cecc0b52d46d + le cast en type "unknow" (selon mongoose) et ensuite le cast en type voulu qui est DocumentType<Category>
     brand: 'Airneis',
     price: 200,
     description: 'Blue 3 seater sofa',
   },
   {
     name: 'NESSIE',
+    slug: 'nessie',
     URLimage: '../public/images/red-2-seater-sofa-in-living-room.png',
-    category: new Category("Sofa", "./images/categories/sofa"),
+    category: (CategoryModel.findById("658ad8a0c5f1cecc0b52d46d")) as unknown as DocumentType<Category>,
     brand: 'Airneis',
     price: 150,
     description: 'Red 2 seater sofa',
   },
   {
     name: 'THISTLE',
+    slug: 'thistle',
     URLimage: '../public/images/single-seat-green-sofa-in-living-room.png',
-    category: new Category("Sofa", "./images/categories/sofa"),
+    category: (CategoryModel.findById("658ad8a0c5f1cecc0b52d46d")) as unknown as DocumentType<Category>,
     brand: 'Airneis',
     price: 100,
     description: 'Green 1 seater sofa',
   },
   {
     name: 'GLEN',
+    slug: 'glen',
     URLimage: '../public/images/modern-brown-coffee-table (1).png',
-    category: new Category("Table", "./images/categories/table"),
+    category: (CategoryModel.findById("658ad8a0c5f1cecc0b52d46d")) as unknown as DocumentType<Category>,
     brand: 'Airneis',
     price: 80,
     description: 'Brown coffee table',
   },
   {
     name: 'WALLACE',
+    slug: 'wallace',
     URLimage:
       '../public/images/bookcase-with-a-wooden-finish-and-a-metal-frame.png',
-    category: new Category("Bookcase", "./images/categories/sofa"),
+    category: (CategoryModel.findById("658ad8a0c5f1cecc0b52d46d")) as unknown as DocumentType<Category>,
     brand: 'Airneis',
     price: 80,
     description: 'bookcase with a wooden finish and a metal frame.',
   },
   {
     name: 'HEATHER',
+    slug: 'heather',
     URLimage: '../public/images/modern-purple-rug-with-floral-pattern.png',
-    category: new Category("Rugs", "./images/categories/rugs"),
+    category: (CategoryModel.findById("658ad8a0c5f1cecc0b52d46d")) as unknown as DocumentType<Category>,
     brand: 'Airneis',
     price: 35,
     description: 'rug with a woolen texture and a purple colour',
   },
   {
     name: 'STIRLING',
+    slug: 'stirling',
     URLimage:
       '../public/images/ad-of-a-lamp-with-glass-shade-and-thin-silver-base.png',
-    category: new Category("Lamps", "./images/categories/lamp"),
+    category: (CategoryModel.findById("658ad8a0c5f1cecc0b52d46d")) as unknown as DocumentType<Category>,
     brand: 'Airneis',
     price: 20,
     description: 'lamp with glass shade and thin silver base',
   },
   {
     name: 'SKYE',
+    slug: 'skye',
     URLimage: '../public/images/wardrobe-with-a-white-colour-and-3-doors.png',
-    category: new Category("Wardrobe", "./images/categories/wardrobe"),
+    category: (CategoryModel.findById("658ad8a0c5f1cecc0b52d46d")) as unknown as DocumentType<Category>,
     brand: 'Airneis',
     price: 200,
     description: 'wardrobe with a white colour and a mirrored door',
