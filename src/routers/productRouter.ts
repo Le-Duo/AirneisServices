b039a63fd011a1d1f6585889e52aba52880e5427
NeverslowLVII <sofiane57800ben@gmail.com>
2024-02-15 22:59:31 +0100

    using body instead of parameters

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -4,87 +4,92 @@
  * La route 'GET /' renvoie tous les produits.
  * La route 'GET /slug/:slug' renvoie les détails d'un produit spécifique.
  */
-import express from "express";
-import asyncHandler from "express-async-handler";
-import { ProductModel } from "../models/product";
-import { CategoryModel } from "../models/category";
-import { isAuth } from "../utils";
-import { Types } from "mongoose";
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import { ProductModel } from '../models/product'
+import { CategoryModel } from '../models/category'
+import { isAuth } from '../utils'
+import { Types } from 'mongoose'
 
-export const productRouter = express.Router();
+export const productRouter = express.Router()
 productRouter.get(
-  "/",
+  '/',
   asyncHandler(async (req, res) => {
-    const products = await ProductModel.find();
-    res.json(products);
+    const products = await ProductModel.find()
+    res.json(products)
   })
-);
+)
 
 productRouter.get(
-  "/slug/:slug",
+  '/slug/:slug',
   asyncHandler(async (req, res) => {
-    const product = await ProductModel.findOne({ slug: req.params.slug });
+    const product = await ProductModel.findOne({ slug: req.params.slug })
     if (product) {
-      res.json(product);
+      res.json(product)
     } else {
-      res.status(404).json({ message: "Product not found" });
+      res.status(404).json({ message: 'Product not found' })
     }
   })
-);
-
-productRouter.get("/search", asyncHandler(async (req, res) => {
-  const searchText = req.query.searchText; // Assuming you're passing the search text as a query parameter
-
-  // Define the aggregation pipeline
-  const pipeline = [
-    {
-      $search: {
-        index: 'searchIndex', // The name of your Atlas Search index
-        text: {
-          query: searchText,
-          path: ['description', 'materials', 'name'], // Fields to search over
-          fuzzy: {} // Optional fuzzy matching
-        }
-      }
-    },
-    {
-      $limit: 10 // Limit the results to 10 documents
-    },
-    {
-      $project: { // Define which fields to include or exclude in the final result
-        _id: 1,
-        name: 1,
-        description: 1,
-        price: 1,
-        URLimage: 1
-      }
-    }
-  ];
-
-  // Execute the aggregation pipeline
-  const results = await ProductModel.aggregate(pipeline).exec();
-
-  // Send the results back to the client
-  res.json(results);
-}));
+)
 
 productRouter.post(
-  "/",
+  '/search',
+  asyncHandler(async (req, res) => {
+    const { searchText } = req.body // Expecting searchText in the request body
+
+    const pipeline = [
+      {
+        $search: {
+          index: 'searchIndex',
+          text: {
+            query: searchText,
+            path: ['description', 'materials', 'name'],
+            fuzzy: {},
+          },
+        },
+      },
+      { $limit: 10 },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          price: 1,
+          URLimage: 1,
+        },
+      },
+    ]
+
+    const results = await ProductModel.aggregate(pipeline).exec()
+    res.json(results)
+  })
+)
+
+productRouter.post(
+  '/',
   // isAuth,
   asyncHandler(async (req, res) => {
     try {
-      const { name, slug, URLimage, categoryId, description, materials, price, priority } =
-        req.body;
+      const {
+        name,
+        slug,
+        URLimage,
+        categoryId,
+        description,
+        materials,
+        price,
+        priority,
+      } = req.body
 
       // Récupère le produit dans la db
-      const category = await CategoryModel.findById(categoryId);
+      const category = await CategoryModel.findById(categoryId)
 
       if (category) {
-        console.log("Category found:", category);
+        console.log('Category found:', category)
       } else {
         // si la catégorie n'existe pas, il ne faut pas aller plus loin
-        console.error("Category not found");
-        res.status(500).json("Category does not exists");
+        console.error('Category not found')
+        res.status(500).json('Category does not exists')
       }
 
       const newProduct = new ProductModel({
@@ -96,65 +101,65 @@ productRouter.post(
         materials,
         price,
         priority,
-      });
+      })
 
       // Enregistrez le produit dans la base de données
-      const savedProduct = await newProduct.save();
+      const savedProduct = await newProduct.save()
 
       // Répondez avec le produit créé
-      res.status(201).json(savedProduct);
+      res.status(201).json(savedProduct)
     } catch (error) {
       // Gérez les erreurs
-      console.error(error);
-      res.status(500).json({ error: "Error on product creation" });
+      console.error(error)
+      res.status(500).json({ error: 'Error on product creation' })
     }
   })
-);
+)
 
 productRouter.delete(
-  "/:id",
+  '/:id',
   asyncHandler(async (req, res) => {
-    const id = req.params.id; // récupère l'id dans les paramètres de l'url
+    const id = req.params.id // récupère l'id dans les paramètres de l'url
 
-    const filtreSuppression = { _id: new Types.ObjectId(id) }; // filtre sur l'id pour la suppression
+    const filtreSuppression = { _id: new Types.ObjectId(id) } // filtre sur l'id pour la suppression
 
     try {
-      const resultat = await ProductModel.deleteOne(filtreSuppression);
+      const resultat = await ProductModel.deleteOne(filtreSuppression)
 
       if (resultat.deletedCount && resultat.deletedCount > 0) {
-        res.json({ message: "product deleted successfully." });
+        res.json({ message: 'product deleted successfully.' })
       } else {
-        res.status(500).json({ error: "Product not found." });
+        res.status(500).json({ error: 'Product not found.' })
       }
     } catch (erreur) {
-      console.error("Error :", erreur);
-      res.status(500).json({ error: "Delete error." });
+      console.error('Error :', erreur)
+      res.status(500).json({ error: 'Delete error.' })
     }
   })
-);
+)
 
 productRouter.put(
-  "/:productId",
+  '/:productId',
   asyncHandler(async (req, res) => {
-    const productId = req.params.productId;
-    const newData = req.body;
+    const productId = req.params.productId
+    const newData = req.body
     try {
       const result = await ProductModel.updateOne(
         { _id: productId },
         { $set: newData }
-      );
+      )
 
       if (result.matchedCount == 0) {
-        res.status(500).json({ error: "No product found" });
+        res.status(500).json({ error: 'No product found' })
       }
       if (result.modifiedCount > 0) {
-        res.status(200).json({ message: "update succeeded" });
+        res.status(200).json({ message: 'update succeeded' })
       }
 
-      res.status(500).json({ error: "Update error" });
+      res.status(500).json({ error: 'Update error' })
     } catch (erreur) {
-      console.error("error :", erreur);
-      res.status(500).json({ error: "Update  error" });
+      console.error('error :', erreur)
+      res.status(500).json({ error: 'Update  error' })
     }
   })
-);
+)
