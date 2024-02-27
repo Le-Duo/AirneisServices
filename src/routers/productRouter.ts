590edea8d3766c64ad91ae4b44634763563da06c
sofiane <129007459+NeverslowLVII@users.noreply.github.com>
2024-02-27 15:21:02 +0100

    default sorting

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -18,6 +18,10 @@ productRouter.get(
   '/',
   asyncHandler(async (req: Request, res: Response) => {
     const products = await ProductModel.find()
+      .sort({
+        priority: -1, // Tri décroissant par priorité (les produits avec une priorité élevée apparaissent en premier)
+        'stock.quantity': -1 // Tri décroissant par quantité en stock (les produits épuisés apparaissent en dernier)
+      })
     res.json(products)
   })
 )
