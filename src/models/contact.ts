56548b296394dadca9855fedec22d3505c25c620
NeverslowLVII <sofiane57800ben@gmail.com>
2024-03-04 00:04:36 +0100

    contact id

--- src/models/contact.ts
+++ src/models/contact.ts
@@ -1,17 +1,18 @@
-import { modelOptions, prop, getModelForClass } from "@typegoose/typegoose";
+import { modelOptions, prop, getModelForClass } from '@typegoose/typegoose'
 
-@modelOptions({ schemaOptions: { collection: "contacts" } })
+@modelOptions({ schemaOptions: { collection: 'contacts' } })
 export class Contact {
+  @prop()
+  public _id?: string
 
   @prop({ required: true })
-  public mail!: string;
+  public mail!: string
 
   @prop({ required: true })
-  public subject!: string;
+  public subject!: string
 
   @prop({ required: true })
-  public message!: string;
-
+  public message!: string
 }
 
-export const ContactModel = getModelForClass(Contact);
+export const ContactModel = getModelForClass(Contact)
