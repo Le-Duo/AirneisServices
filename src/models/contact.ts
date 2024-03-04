c87addff83c0aec236ce207385319455e59bd2b5
sofiane <129007459+NeverslowLVII@users.noreply.github.com>
2024-03-04 11:10:54 +0100

    test required false

--- src/models/contact.ts
+++ src/models/contact.ts
@@ -3,7 +3,8 @@ import { User } from './user'
 
 @modelOptions({ schemaOptions: { collection: 'contacts' } })
 export class Contact {
-  
+
+  @prop({ required: false })
   public _id?: string
 
   @prop({ required: true })
